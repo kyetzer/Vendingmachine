aeb5ae63daafc2fce5f28658f132b59e135b29ed
kyetz <kyetz@DESKTOP-BRUBHRM>
2016-01-27 21:50:23 -0500

    A money class to handle the accumulation of money.  First method is Return the Coin Dollar Amount given Weight and Size.  There are only 4 entries,  3 valid:  nickle, dime, quarter and 1 invalid: other

---
+++ src/vendingmachine/money.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vendingmachine;
+
+public class money {
+   private double accumamount=0;
+   private int coinsadded=0;
+   private int coinsrejected=0;
+   
+   Double RetCoinAmt(double wgt,double size){
+        Double retval=0.0;
+        
+        for (Coins c : Coins.values()){
+           if ((c.weight() == wgt) && (c.size() == size)){
+               retval = c.dollaramt();
+               break;
+           }
+        }
+        return retval;
+    }
+}
