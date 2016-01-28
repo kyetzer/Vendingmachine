f55485479b79426e0a7ee85a561dae4323df49c7
kyetz <kyetz@DESKTOP-BRUBHRM>
2016-01-27 21:30:14 -0500

    Enum Object for Displaying Messages

---
+++ src/vendingmachine/DisplayedMessages.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vendingmachine;
+
+public enum DisplayedMessages {
+    Insert    ("Insert Coins",0),     //  $0.00
+    Price     ("Price       ",1),
+    Thankyou  ("Thank You   ",2);
+
+    private final String msg; 
+    private final int msgnumb;
+    
+    DisplayedMessages(String msg,int msgnumb) {
+        this.msg = msg;
+        this.msgnumb = msgnumb;
+    }
+    public int Msgnumb(){ return msgnumb; }
+    public String BasicMessage() { return msg; }
+    public String ModMessage(double inval){ 
+        if (inval < 0.0) return msg + "     ";
+        else return msg + " $" + inval;
+    } 
+}
