9053dfd6f4051d1d02afc78a20777c5a46269ab7
kyetz <kyetz@DESKTOP-BRUBHRM>
2016-01-27 23:04:36 -0500

    Added an availability flag

--- src/vendingmachine/Products.java
+++ src/vendingmachine/Products.java
@@ -6,17 +6,20 @@
 package vendingmachine;
 
 public enum Products {
-    COLA    (1.0,0),
-    CHIPS   (0.50,1),
-    CANDY   (0.65,2);
+    COLA    (1.0,0,true),
+    CHIPS   (0.50,1,true),
+    CANDY   (0.65,2,true);
 
     private final double cost;   
     private final int but;
-    Products(double cost,int but) {
+    private final boolean avail;
+    Products(double cost,int but,boolean avail) {
         this.cost = cost;
         this.but = but;
+        this.avail = avail;
     }
     public double cost() { return cost; }
     public int but(){ return but; }
     public String cname(){ return this.name(); }
+    public boolean itemavail(){ return avail; }
 }
